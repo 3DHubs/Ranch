e87a8a80548d7bb6f0f7196a5c1283cddc5a2f96
Martijn Arts <arts.martijn@gmail.com>
2016-03-29 16:08:34 +0200

    Add classifiers, url, description to setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,17 @@ from setuptools import setup
 setup(
     name='Ranch',
     version='0.1.0',
-    description='Addressing done right',
+    description='Ranch does addressing in Python',
     author='Martijn Arts',
     author_email='martijn@3dhubs.com',
+    url="https://github.com/3DHubs/ranch",
     packages=['ranch'],
     scripts=['scripts/ranch-download'],
-    install_requires=['beautifulsoup4', 'flake8', 'requests']
+    install_requires=['beautifulsoup4', 'flake8', 'requests'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+    ]
 )
