a04d8c10fa3bae3610bd86a8e8f3ef0d73e00dbf
Martijn Arts <arts.martijn@gmail.com>
2016-03-30 18:09:54 +0200

    Properly set up dependencies

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
-
 from setuptools import setup
+
+import pypandoc
+long_description = pypandoc.convert('README.md', 'rst')
 
 setup(
     name='Ranch',
     version='0.1.0',
     description='Ranch does addressing in Python',
+    long_description=long_description,
     author='Martijn Arts',
     author_email='martijn@3dhubs.com',
     url="https://github.com/3DHubs/ranch",
     packages=['ranch'],
     scripts=['scripts/ranch-download'],
-    install_requires=['beautifulsoup4', 'flake8', 'requests'],
+    install_requires=['beautifulsoup4', 'flake8', 'requests', 'progressbar2',
+                      'percache'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: Apache Software License',
