26d2e78ab9066378cc0416ad9e20e4195c015262
Martijn Arts <arts.martijn@gmail.com>
2016-04-14 16:26:49 +0200

    Bump version!

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='Ranch',
-    version='0.2.4',
+    version='0.2.5',
     description='Ranch does addressing in Python',
     long_description=long_description,
     author='Martijn Arts',
